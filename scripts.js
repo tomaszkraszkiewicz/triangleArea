df4b2567b53214cdf3bc05d02920205dad745b55
tomaszkraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-04-25 12:55:10 +0200

    js base structure added

--- scripts.js
+++ scripts.js
@@ -1,12 +1,4 @@
-<html> 
-	<head> 
-		<meta charset="utf-8"> 
-		<title> Triangle Area </title> 
-	</head> 
-		<body> 
-			<div class="container">
-				<h1> Pole Trójkąta </h1> 
-			</div>
-			<script type="text/javascript" src="js/scripts.js"></script> 
-		</body> 
-</html>
+var a = 2;
+var h = 4;
+var triangleArea = a*h/2
+
