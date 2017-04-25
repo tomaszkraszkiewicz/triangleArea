7cd3375717ce569b31c6ff24873e6c271d60895f
tomaszkraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-04-25 12:52:01 +0200

    task no 8_5 base files

---
+++ scripts.js
@@ -0,0 +1,12 @@
+<html> 
+	<head> 
+		<meta charset="utf-8"> 
+		<title> Triangle Area </title> 
+	</head> 
+		<body> 
+			<div class="container">
+				<h1> Pole Trójkąta </h1> 
+			</div>
+			<script type="text/javascript" src="js/scripts.js"></script> 
+		</body> 
+</html>
