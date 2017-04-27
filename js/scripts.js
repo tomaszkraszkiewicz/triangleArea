60bfb4275ed8860b18d1b4d8d21e0c93534f3805
tomaszkraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-04-27 15:47:54 +0200

    changes in script - prompt added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = 2;
-var h = 4;
-var triangleArea = a*h/2
+var a = prompt('Podaj liczbę');
+var h = prompt('podaj liczbę');
+var triangleArea = a*h/2;
 
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
